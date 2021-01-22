3520bca8333f6442a74b9ba5cc114e0ef2757230
Anna-Erika Nurmi <anna-erika.nurmi@helsinki.fi>
2021-01-22 13:56:01 +0200

    changed my program to being a tad more advanced

--- annastest.py
+++ annastest.py
@@ -1,1 +1,5 @@
-print("Hello World!")
+question = input("Leaving or arriving? Answer with L for leaving and A for arriving.")
+if question == "L":
+	print("Good bye!")
+if question == "A":
+	print("Welcome!")

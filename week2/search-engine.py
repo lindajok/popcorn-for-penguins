ac9396cd549670a366dd0ed718aca24e54496cd8
Martina Koutová <koutova.maja@outlook.com>
2021-01-29 19:57:27 +0200

    I screwed the renaming of this file up but now it should work probably?

---
+++ week2/search-engine.py
@@ -0,0 +1,46 @@
+from sklearn.feature_extraction.text import CountVectorizer
+
+documents = ["This is a silly example",
+             "A better example",
+             "Nothing to see here",
+             "This is a great and long example"]
+
+
+cv = CountVectorizer(lowercase=True, binary=True)
+sparse_matrix = cv.fit_transform(documents)
+dense_matrix = sparse_matrix.todense()
+td_matrix = dense_matrix.T
+sparse_td_matrix = sparse_matrix.T.tocsr()
+t2i = cv.vocabulary_
+
+d = {"and": "&", "AND": "&",
+     "or": "|", "OR": "|",
+     "not": "1 -", "NOT": "1 -",
+     "(": "(", ")": ")"} 
+
+def rewrite_token(t):
+    return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t)) # Make retrieved rows dense
+
+def rewrite_query(query): # rewrite every token in the query
+    return " ".join(rewrite_token(t) for t in query.split())
+
+def test_query(query):
+    print("Query: '" + query + "'")
+    print("Rewritten:", rewrite_query(query))
+    print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
+    print()
+
+def print_results():
+    hits_matrix = eval(rewrite_query(user_input)) #!
+    hits_list = list(hits_matrix.nonzero()[1])
+    for i, doc_idx in enumerate(hits_list):
+        print("Matching doc #{:d}: {:s}".format(i, documents[doc_idx]))
+
+user_input = "0"
+while user_input != "":
+    user_input = input("Write the query (to stop press Enter): ")
+    if user_input == "":
+        break
+    else:
+        test_query(user_input)
+        print_results()

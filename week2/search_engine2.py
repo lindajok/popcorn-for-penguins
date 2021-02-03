a3becde9e39ce440c268e1930671d3513b86b335
lindajok <linda.jokinen@helsinki.fi>
2021-02-03 20:40:28 +0200

    #11 Add list of words not in the documents

--- week2/search_engine2.py
+++ week2/search_engine2.py
@@ -1,11 +1,10 @@
+from numpy.matrixlib.defmatrix import matrix
 from sklearn.feature_extraction.text import CountVectorizer
 from bs4 import BeautifulSoup
 import io
 
-
 def prepare_data():
     """ Read a file and make a list of strings """
-    
     documents = []
     article = ""
     f = io.open("100.txt", mode="r", encoding="utf-8" )    # Change the name if you want to test with the 1000 article file
@@ -25,9 +24,7 @@ def prepare_data():
     
     return documents
 
-
 documents = prepare_data() # Documents in a list of strings format, left as a global variable
-
 
 # This is the code we had earlier -->
 # We could eventually put it into a function too
@@ -38,7 +35,6 @@ td_matrix = dense_matrix.T
 sparse_td_matrix = sparse_matrix.T.tocsr()
 t2i = cv.vocabulary_ # dictionary of terms
 
-
 def rewrite_token(t):
     d = {"and": "&", "AND": "&",
      "or": "|", "OR": "|",
@@ -47,23 +43,23 @@ def rewrite_token(t):
 
     return d.get(t, 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t)) # Make retrieved rows dense
 
-
 def rewrite_query(query): # rewrite every token in the query
     return " ".join(rewrite_token(t) for t in query.split())
 
+def print_results(user_input):
+    not_in_dict = []
+    for w in user_input.split():
+        try:    
+            hits_matrix = eval(rewrite_query(w))
+            hits_list = list(hits_matrix.nonzero()[1])
+        except:
+            not_in_dict.append(w)
 
-# This function is just for testing and interferes when printing:
-# def test_query(query):
-#     print("Query: '" + query + "'")
-#     print("Rewritten:", rewrite_query(query))
-#     print("Matching:", eval(rewrite_query(query))) # Eval runs the string as a Python command
-#     print()
-
-
-def print_results():
     try:
         hits_matrix = eval(rewrite_query(user_input))
+        # print(hits_matrix)
         hits_list = list(hits_matrix.nonzero()[1])
+        # print("LIST hits_matrix", list(hits_matrix))
         print("Total number of matching documents: {:d}".format(len(hits_list)))
         for i, doc_idx in enumerate(hits_list):
             if i > 4:
@@ -71,8 +67,7 @@ def print_results():
             else:
                 print("Example of a matching doc #{:d}: {:s}...".format(i, documents[doc_idx][:50]))
     except:
-        print("Oops, someting went wrong.")
-
+        print("Oops, something went wrong.")
 
 user_input = "0"
 while user_input != "":
@@ -81,6 +76,6 @@ while user_input != "":
         if user_input == "":
             break
         else:
-            print_results()
+            print_results(user_input)
     except KeyError:
         print("Bad query")

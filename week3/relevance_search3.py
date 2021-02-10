f17b93778920bc1deffffca9438fd147a7cb1b5e
lindajok <linda.jokinen@helsinki.fi>
2021-02-11 00:00:42 +0200

    Add user input stemming to a function

--- week3/relevance_search3.py
+++ week3/relevance_search3.py
@@ -20,6 +20,14 @@ def stemming(documents):
     return documents
 
 
+def stemming_string(user_input):
+    user_input = nltk.word_tokenize(user_input)
+    user_input = [stemmer.stem(word) for word in user_input]
+    user_input = ' '.join([word for word in user_input])
+
+    return user_input
+
+
 def title_list():
     """ Read a file and make a list with titles of the document """
     title_list = []
@@ -58,16 +66,19 @@ def prepare_data():
 
 # The original output from the tutorial:
 def print_results(user_input):
-    documents = stemming(prepare_data())
+    # documents = stemming(prepare_data())
+    documents = prepare_data()
+    stemmed_documents = stemming(documents)
+
     tfv5 = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2")
-    sparse_matrix = tfv5.fit_transform(documents).T.tocsr() # CSR: compressed sparse row format => order by terms
+    sparse_matrix = tfv5.fit_transform(stemmed_documents).T.tocsr() # CSR: compressed sparse row format => order by terms
     query_vec5 = tfv5.transform([user_input]).tocsc()
     hits = np.dot(query_vec5, sparse_matrix)
 
     ranked_scores_and_doc_ids = sorted(zip(np.array(hits[hits.nonzero()])[0], hits.nonzero()[1]), reverse=True)
     for score, i in ranked_scores_and_doc_ids:
         first_occurrence = documents[i].find(user_input)
-        print("The score of {} is {:.4f} in document: {:s}".format(user_input, score, documents[i][first_occurrence:first_occurrence+50]))
+        print("The score of {} is {:.4f} in document: {:s}".format(user_input, score, stemmed_documents[i][first_occurrence:first_occurrence+50]))
 
 
 ######################################################################
@@ -98,7 +109,7 @@ def print_results(user_input):
 # ---------------------- New code here too -------------------------
 
 #tokenize(prepare_data()) # calling the function tokenize
-stemming(prepare_data()) # calling the function stemming --> stemming(tokenize(prepare_data)))
+# stemming(prepare_data()) # calling the function stemming --> stemming(tokenize(prepare_data)))
 # --> I really don't know what to do with them next ((⇀‸↼))
 # You can try to print these two functions with a smaller dataset so
 # you know how the output looks like. Also this implementation is
@@ -111,9 +122,7 @@ def main():
     user_input = "0"
     while user_input != "":
         user_input = input("Write the query (press enter to stop): ").lower()
-        user_input = nltk.word_tokenize(user_input)
-        user_input = [stemmer.stem(word) for word in user_input]
-        user_input = ' '.join([word for word in user_input])
+        user_input = stemming_string(user_input)
         try:
             if user_input == "":
                 break

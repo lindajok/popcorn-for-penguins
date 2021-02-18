08a56745ba0bcf5f55d0a7935cb904226eed3958
Anna-Erika Nurmi <anna-erika.nurmi@helsinki.fi>
2021-02-18 11:51:06 +0200

    deleted and added you know the drill

--- week2/upper_boolean.py
+++ week2/upper_boolean.py
@@ -1,25 +1,25 @@
 # This is a version of the Boolean search engine which only supports uppercase Boolean operators.
 
 from sklearn.feature_extraction.text import CountVectorizer
-from bs4 import BeautifulSoup
 import io
+import re
 
 def prepare_data():
     """ Read a file and make a list of strings """
     documents = []
     article = ""
-    f = io.open("data100.txt", mode="r", encoding="utf-8" )                 # Change the name if you want to change the dataset 
+    f = io.open("data100.txt", mode="r", encoding="utf-8")
     for line in f:
-        line = line.replace('\n', ' ')                                      # Replace newline characers with space
-        if line == "</article> ":                                           # Locate boundaries between articles
-                clean_version = BeautifulSoup(article, "html.parser").text  # Remove tags
-                documents.append(clean_version)                             # Add the article string without tags to the list of documents
-                article=""                                                  # Define "auxiliary" variable again
+        line = line.replace('\n', ' ')
+        if line == "</article> ":
+            clean = re.compile('<.*?>')
+            clean_version = re.sub(clean, '', article)
+            documents.append(clean_version)
+            article=""
         else:
-                article+=line                                                                        
+            article+=line
     f.close()
     return documents
-
 
 documents = prepare_data() # Documents in a list of strings format, left as a global variable
 
@@ -40,19 +40,7 @@ def rewrite_token(t):
 def rewrite_query(query): # rewrite every token in the query
     return " ".join(rewrite_token(t) for t in query.split())
 
-def input_string(user_input): # tool for handling unknown terms
-    string = user_input
-    string = string.replace(' AND ', ' ')
-    string = string.replace(' OR ', ' ')
-    string = string.replace('NOT ', '')
-    string = string.replace(' NOT ', ' ')
-    li = string.split(' ')
-    return li
-
 def print_results(user_input):
-    for i in input_string(user_input): # PRINTING
-        if i not in t2i: # IF TERM
-            print("Given term '{}' not found in any document.".format(i)) # NOT FOUND
     hits_matrix = eval(rewrite_query(user_input))
     hits_list = list(hits_matrix.nonzero()[1])
     print("Total number of matching documents: {:d}".format(len(hits_list)))

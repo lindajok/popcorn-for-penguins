701914e5b0453188f4391f9abeeeac02e74720f6
Martina Koutová <koutova.maja@outlook.com>
2021-02-22 21:47:37 +0200

    #22 Future wildcard search code

---
+++ week3/trie.py
@@ -0,0 +1,57 @@
+# https://www.riff.ie/2020/02/05/wildcard-searching-trie.html
+from typing import Dict, List, Any
+
+WILDCARD = '?'
+
+class Trie:
+    def __init__(self):
+        self.children : Dict[str, Trie] = {}
+        self.isLeaf: bool = False
+
+    def insert(self, word: str) -> None:
+        node = self
+        for char in word:
+            if char not in node.children:
+                node.children[char] = Trie()
+            node = node.children[char]
+        node.isLeaf = True
+
+    def find(self, word: str) -> bool:
+        stack = [(self, 0, '')]
+        result = []
+
+        while stack:
+            curr, count, currWord = stack.pop()
+
+            if count == len(word):
+                if curr.isLeaf: result.append(currWord)
+                continue
+
+            currChar = word[count]
+            if currChar == WILDCARD:
+                for childChar, node in curr.children.items():
+                    stack.append((node, count + 1, currWord + childChar))
+                continue
+
+            if currChar in curr.children:
+                node = curr.children[currChar]
+                stack.append((node, count + 1, currWord + currChar))
+
+        return result
+
+def main():
+    trie = Trie()
+    trie.insert('word')
+    trie.insert('ward')
+    trie.insert('oi')
+    trie.insert('boi')
+    
+    print(trie.find('??'))
+    assert(trie.find('??') == ['oi'])
+    assert(trie.find('') == [])
+    assert(trie.find('w?rd') == ['ward', 'word'])
+    assert(trie.find('???') == ['boi'])
+    assert(trie.find('?oi') == ['boi'])
+
+if __name__ == '__main__':
+    main()

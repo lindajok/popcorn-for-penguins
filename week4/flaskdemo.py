b76cc723543455b7c58e72f6420648bf5283e72f
Martina Koutová <koutova.maja@outlook.com>
2021-02-23 16:20:43 +0200

    #26 Flask now works with the basic functionality of boolean search

--- week4/flaskdemo.py
+++ week4/flaskdemo.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request
 import io
-
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import re
 import numpy as np
 import nltk
@@ -31,6 +30,44 @@ def prepare_data():
 
 documents = prepare_data()
 
+cv = CountVectorizer(lowercase=True, binary=True, token_pattern=r'(?u)\b\w+\b')
+sparse_matrix = cv.fit_transform(documents)
+dense_matrix = sparse_matrix.todense()
+td_matrix = dense_matrix.T
+sparse_td_matrix = sparse_matrix.T.tocsr()
+t2i = cv.vocabulary_ # dictionary of terms
+terms = cv.get_feature_names()
+
+tfv5 = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2", token_pattern=r'(?u)\b\w+\b')
+sparse_matrix = tfv5.fit_transform(documents).T.tocsr()
+
+def rewrite_token(t):
+    d = {"AND": "&",
+     "OR": "|",
+     "NOT": "1 -",
+     "(": "(", ")": ")"}
+    if t in d:
+        return d.get(t)
+    elif t.lower() in terms:
+        return 'sparse_td_matrix[t2i["{:s}"]].todense()'.format(t.lower())
+    else:
+        return 'np.matrix(np.zeros(len(documents), dtype=int))'
+
+def rewrite_query(query): # rewrite every token in the query
+    return " ".join(rewrite_token(t) for t in query.split())
+
+def print_results(user_input):
+    matches = []
+    try:
+        hits_matrix = eval(rewrite_query(user_input))
+        hits_list = list(hits_matrix.nonzero()[1])
+        for i, doc_idx in enumerate(hits_list):
+            matches.append("Example of a matching doc #{:d}: {:s}...".format(i, documents[doc_idx][:50]))
+    except:
+        matches.append("No documents found")
+    return matches
+
+
 #Function search() is associated with the address base URL + "/search"
 @app.route('/search')
 def search():
@@ -42,11 +79,7 @@ def search():
 
     #If query exists (i.e. is not None)
     if query:
-        #Look at each entry in the example data
-        for entry in documents:
-            #If an entry name contains the query, add the entry to matches
-            if query.lower() in entry.lower():
-                matches.append(entry)
+        matches = print_results(query)
 
     #Render index.html with matches variable
     return render_template('index.html', matches=matches)

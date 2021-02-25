2ed885681ba836c4f49931ce5e237d06ed309718
lindajok <59133469+lindajok@users.noreply.github.com>
2021-02-25 19:56:19 +0200

    Documents in a list of dictionaries format
    + selection bar that doesn't work and datetime addition

--- tomato_engine/search_engine.py
+++ tomato_engine/search_engine.py
@@ -3,10 +3,12 @@ import io
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 import re
 import numpy as np
-import nltk
-from nltk.stem import PorterStemmer
-from nltk.tokenize import sent_tokenize, word_tokenize
-import copy
+
+# from nltk.stem import PorterStemmer
+# from nltk.tokenize import sent_tokenize, word_tokenize
+# import copy
+
+from datetime import datetime
 
 #Initialize Flask instance
 app = Flask(__name__)
@@ -19,8 +21,7 @@ def prepare_data():
     for line in f:
         line = line.replace('\n', ' ')
         if line == "</article> ":
-            clean = re.compile('<.*?>')
-            clean_version = re.sub(clean, '', article)
+            clean_version = re.sub("</article>", '', article)
             documents.append(clean_version)
             article=""
         else:
@@ -41,6 +42,7 @@ terms = cv.get_feature_names()
 tfv5 = TfidfVectorizer(lowercase=True, sublinear_tf=True, use_idf=True, norm="l2", token_pattern=r'(?u)\b\w+\b')
 sparse_matrix = tfv5.fit_transform(documents).T.tocsr()
 
+
 def rewrite_token(t):
     d = {"AND": "&",
      "OR": "|",
@@ -53,21 +55,33 @@ def rewrite_token(t):
     else:
         return 'np.matrix(np.zeros(len(documents), dtype=int))'
 
+
 def rewrite_query(query): # rewrite every token in the query
     return " ".join(rewrite_token(t) for t in query.split())
 
-def print_results(user_input):
+
+def format(user_input):
     matches = []
+    dict = {}
     hits_matrix = eval(rewrite_query(user_input))
     hits_list = list(hits_matrix.nonzero()[1])
     for i, doc_idx in enumerate(hits_list):
-        matches.append("Example of a matching doc #{:d}: {:s}...".format(i, documents[doc_idx][:50]))    
+        title = re.sub(r'<article name="', '', documents[doc_idx])
+        title = re.sub('">.*', '', title)
+        content = re.sub('<.*?>', '', documents[doc_idx])
+        dict[title]=content
+        matches.append(dict)  
     return matches
 
 
 #Function search() is associated with the address base URL + "/search"
-@app.route('/search')
+@app.route('/search', methods=['GET', 'POST'])
 def search():
+    today = datetime.today()
+    day_name = today.strftime("%A")
+
+    mealtypes = ['-', 'Starter', 'Main', 'Dessert']
+
     #Get query from URL variable
     query = request.args.get('query')
 
@@ -76,7 +90,7 @@ def search():
 
     #If query exists (i.e. is not None)
     if query:
-        matches = print_results(query)
+        matches = format(query)
 
     #Render index.html with matches variable
-    return render_template('index.html', matches=matches)
+    return render_template('index.html', matches=matches, day=day_name, mealtypes=mealtypes)
